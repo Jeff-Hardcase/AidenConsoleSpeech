e85ffd55d56e3a7ed9a0d6247f0a9805185e1512
Jeff-Hardcase <32984988+Jeff-Hardcase@users.noreply.github.com>
2021-06-18 09:25:27 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace AidenConsoleSpeech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var speak = "Hey Aiden.";
+            Console.WriteLine(speak);
+
+            using (var synth = new SpeechSynthesizer())
+            {
+                synth.SelectVoice("Microsoft Zira Desktop");
+
+                do
+                {
+                    synth.Speak(speak);
+                    speak = Console.ReadLine();
+                } 
+                while (speak != "exit");
+            }
+        }
+    }
+}
